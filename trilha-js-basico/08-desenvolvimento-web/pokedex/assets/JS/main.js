859cb6490db51c99b41a0b8f46fab49b4ba36770
iagoxxpro1 <yagoxx@hotmail.com>
2022-10-27 14:15:12 -0300

    finalização da aula de conhecimento sobre protocolo HTTP e integração com a PokeAPI

--- trilha-js-basico/08-desenvolvimento-web/pokedex/assets/JS/main.js
+++ trilha-js-basico/08-desenvolvimento-web/pokedex/assets/JS/main.js
@@ -1,25 +1,44 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
 
-function convertPokemonToLi(pokemon) {
-    return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
+const maxRecords = 151
+const limit = 10
+let offset = 0;
 
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
-            </ol>
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
 
-            <img src="${pokemon.photo}"
-                alt="${pokemon.name}">
-        </div>
-    </li>
-    `
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('')
+
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToLi).join('')
-    pokemonList.innerHTML = newHtml
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordWithNextPage = offset + limit
+
+    if (qtdRecordWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
 })
--- trilha-js-basico/08-desenvolvimento-web/pokedex/assets/JS/poke-api.js
+++ trilha-js-basico/08-desenvolvimento-web/pokedex/assets/JS/poke-api.js
@@ -3,7 +3,7 @@ const pokeApi = {}
 
 function converPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
@@ -23,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(converPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url) //buscando a nossa lista de pokemons
