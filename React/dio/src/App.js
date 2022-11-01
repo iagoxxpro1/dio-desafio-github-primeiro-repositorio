5a33a8a0b09553c37ca75f46a2f89c81524b4558
iagoxxpro1 <yagoxx@hotmail.com>
2022-11-01 10:34:50 -0300

    remocao de arquivos duplicados e também realocados nas pastas

--- React/dio/src/App.js
+++ React/dio/src/App.js
@@ -1,11 +1,16 @@
-import Button from "./components/button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Olá React</h1>
-      <Button title="Entrar" />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
