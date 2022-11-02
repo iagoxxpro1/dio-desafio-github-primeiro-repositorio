9e1fab101e48db2d8bbb229d12336d97dfc250e6
iagoxxpro1 <yagoxx@hotmail.com>
2022-11-02 17:49:15 -0300

    instalacao npm install react hook form

--- React/dio/src/App.js
+++ React/dio/src/App.js
@@ -4,16 +4,16 @@ import {
   Route,
 } from "react-router-dom";
 
-import { Home } from "./pages/home";
-import { Login } from "./pages/login";
-import {Feed} from './pages/feed'
+import { Home } from "./pages/home"
+import { Login } from './pages/login'
+import { Feed } from './pages/feed'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/feed" element={<Feed />} />
       </Routes>
     </Router>
