41a7bb82d8055adf93c56c93b6a60b24e54a246b
iagoxxpro1 <yagoxx@hotmail.com>
2022-11-02 21:55:23 -0300

    por algum descuido, minha página de login e somente ela bugou. voltando atrás das últimas atualizacoes como tentativa

--- React/dio/src/pages/login/index.jsx
+++ React/dio/src/pages/login/index.jsx
@@ -1,8 +1,4 @@
 import {MdEmail, MdLock} from 'react-icons/md'
-import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
@@ -10,26 +6,7 @@ import { Input } from "../../components/Input";
 
 import {Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper} from './styles';
 
-const schema = yup.object({
-  email: yup.string().email('email não é valido').required('Campo obrigatório'),
-  password: yup.string().min(3, 'No minimo 3 caracteres').required('Campo obrigatório'),
-}).required();
-
 const Login = () => {
-  const navigate = useNavigate();
-
-  const {control, handleSubmit, formState: {errors, isValid} } = useForm({
-      resolver: yupResolver(schema),
-      mode: 'onChange',
-  });
-
-    console.log(isValid, errors);
-
-  const onSubmit = data => console.log(data);
-
-  const handleClickSignIn = () => {
-    navigate('/feed')
-  }
 
   return (<>
       <Header />
@@ -44,10 +21,10 @@ const Login = () => {
           <Wrapper>
             <TitleLogin>Faça o seu cadastro</TitleLogin>
             <SubtitleLogin>Faça o seu login e make the change._</SubtitleLogin>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Input name="email" errorMessage={errors?.email?.message} control={control} placeholder="E-mail" leftIcon={<MdEmail />}/>
-              <Input name="password" errorMessage={errors?.password?.message} control={control} placeholder="Senha" type="password" leftIcon={<MdLock />}/>
-              <Button title="Entrar" variant="secondary" type="submit" />
+            <form>
+              <Input placeholder="E-mail" leftIcon={<MdEmail />}/>
+              <Input placeholder="Senha" type="password" leftIcon={<MdLock />}/>
+              <Button title="Entrar" variant="secondary" />
             </form>
             <Row>
               <EsqueciText>Esqueci minha senha</EsqueciText>
@@ -60,3 +37,4 @@ const Login = () => {
 }
 
 export { Login }
+
