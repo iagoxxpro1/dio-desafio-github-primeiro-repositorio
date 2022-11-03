d8fbd4675016e9191537dfc64d75c30319097521
iagoxxpro1 <yagoxx@hotmail.com>
2022-11-03 18:55:24 -0300

    CORRIGIDO o problema da tela branca após a aula REQUISIÇÃO COM AXIOS

--- React/dio/src/pages/login/index.jsx
+++ React/dio/src/pages/login/index.jsx
@@ -1,40 +1,67 @@
-import {MdEmail, MdLock} from 'react-icons/md'
+import { useNavigate  } from "react-router-dom";
+import { MdEmail, MdLock } from 'react-icons/md'
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { api } from '../../services/api';
 
-import { Button } from "../../components/Button";
-import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
+import { useForm } from "react-hook-form";
 
-import {Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper} from './styles';
+
+import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
 
 const Login = () => {
 
-  return (<>
-      <Header />
-      <Container>
-        <Column>
-          <Title>
-              A plataforma para você aprender com experts, dominar as principais tecnologias
-                e entrar mais rápido nas empresas desejadas.
-          </Title>
-        </Column>
-        <Column>
-          <Wrapper>
-            <TitleLogin>Faça o seu cadastro</TitleLogin>
-            <SubtitleLogin>Faça o seu login e make the change._</SubtitleLogin>
-            <form>
-              <Input placeholder="E-mail" leftIcon={<MdEmail />}/>
-              <Input placeholder="Senha" type="password" leftIcon={<MdLock />}/>
-              <Button title="Entrar" variant="secondary" />
-            </form>
-            <Row>
-              <EsqueciText>Esqueci minha senha</EsqueciText>
-              <CriarText>Criar Conta</CriarText>
-            </Row>
-          </Wrapper>
-        </Column>
-      </Container>
+    const navigate = useNavigate()
+
+    const { control, handleSubmit, formState: { errors  } } = useForm({
+        reValidateMode: 'onChange',
+        mode: 'onChange',
+    });
+
+    const onSubmit = async (formData) => {
+        try{
+            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
+            
+            if(data.length && data[0].id){
+                navigate('/feed') 
+                return
+            }
+
+            alert('Usuário ou senha inválido')
+        }catch(e){
+            //TODO: HOUVE UM ERRO
+        }
+    };
+
+    console.log('errors', errors);
+
+    return (<>
+        <Header />
+        <Container>
+            <Column>
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
+                 e entrar mais rápido nas empresas mais desejadas.</Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                <TitleLogin>Faça seu cadastro</TitleLogin>
+                <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
+                    {errors.email && <span>E-mail é obrigatório</span>}
+                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
+                    {errors.senha && <span>Senha é obrigatório</span>}
+                    <Button title="Entrar" variant="secondary" type="submit"/>
+                </form>
+                <Row>
+                    <EsqueciText>Esqueci minha senha</EsqueciText>
+                    <CriarText>Criar Conta</CriarText>
+                </Row>
+                </Wrapper>
+            </Column>
+        </Container>
     </>)
 }
 
 export { Login }
-
